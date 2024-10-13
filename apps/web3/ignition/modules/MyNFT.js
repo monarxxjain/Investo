151528f7948b7bb851725240a6fc0938ca911ea4
monarxxjain <jainmonark1000@gmail.com>
2024-10-13 18:42:28 +0530

    Contract Deployed

--- apps/web3/ignition/modules/MyNFT.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("LockModule", (m) => {
-
-  const lock = m.contract("MyNFT");
-
-  return { lock };
-});
-
--- apps/web3/ignition/modules/Puma.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("PumaModule", (m) => {
-  
-  const lock = m.contract("Puma", ["0xCe52C63E5030879079c4C5B993A5EE8282a60A88"])
-
-  return { lock };
-});
-
